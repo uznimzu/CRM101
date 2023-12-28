0017de6a34e96de8a463dedb717e3829a31e2ed9
uznimzu <zu_min@naver.com>
2023-12-28 12:24:43 +0900

    청년 CRM 101 _ 깃허브 이클립스 연동하는 방법 학습하기

---
+++ CRM101/src/com/salesforce/Crm101.java
@@ -0,0 +1,10 @@
+package com.salesforce;
+
+public class Crm101 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("청년CRM101-깃허브와 이클립스 연동하기");
+	}
+
+}
